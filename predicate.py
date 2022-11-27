049db2ca92f5f0fb7c7290db1ba7e94df8dbcdf1
Дмитрий <dimon20.07@mail.ru>
2022-11-28 01:19:36 +0300

    predicate

---
+++ predicate.py
@@ -0,0 +1,9 @@
+#Напишите программу для проверки истинности утверждения 
+# ¬(X ? Y ? Z) = ¬X ? ¬Y ? ¬Z для всех значений предикат. 
+# ? - and ? - or ¬ - not
+
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
